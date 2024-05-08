72826dc3f378d3ab465250b13318c7e9778c592c
Aaron C <aaronc@MacBook-Pro-6.local>
2024-05-07 20:25:15 -0400

    discussion page logic

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -14,10 +14,12 @@ export default function SiteHeadingAndNav() {
         {
           currentUser
             ? <>
+              <li><NavLink to={'/discussion'}>Discussions</NavLink></li>
               <li><NavLink to='/users' end={true}>Users</NavLink></li>
               <li><NavLink to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink></li>
             </>
             : <>
+              <li><NavLink to={'/discussion'}>Discussions</NavLink></li>
               <li><NavLink to='/login'>Login</NavLink></li>
               <li><NavLink to='/sign-up'>Sign Up</NavLink></li>
             </>
