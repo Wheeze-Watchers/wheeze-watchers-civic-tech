24c49badeb4d1eddcbef2413cb7b76974f329726
mayayr115 <mayayr115@gmail.com>
2024-05-09 12:42:56 -0400

    Aaron's original dummy data, my changes are in Aaron's last commit

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -1,27 +1,25 @@
-const User = require('../models/User');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-// exports.seed = async (knex) => {
-//   // Before you have models you can always just do `await knex('table_name').del`
-//   await knex('user').del();
-
-//   await knex.raw('ALTER SEQUENCE user_id_seq RESTART WITH 1');
-
-//   await User.create('cool_cat', '1234');
-//   await User.create('l33t-guy', '1234');
-//   await User.create('wowow', '1234');
-// };
-
-exports.seed = function(knex) {
+exports.seed = async (knex) => {
   // Deletes ALL existing entries
   return knex('user').del()
     .then(() => {
       // Inserts seed entries
       return knex('user').insert([
-        {username: 'john_doe', password_hash: 'hashed_password_1'},
+        {first_name: 'bob', last_name: 'dylan', email: 'bobdylan@mail.com', username: 'john_doe', password_hash: 'hashed_password_1', expert: true},
+        {first_name: 'bob', last_name: 'dylan', email: 'bobdylan2@mail.com', username: 'john_doe2', password_hash: 'hashed_password_1', expert: true},
+        {first_name: 'bob', last_name: 'dylan', email: 'bobdylan3@mail.com', username: 'john_doe3', password_hash: 'hashed_password_1', expert: true},
         {username: 'jane_doe', password_hash: 'hashed_password_2'}
-      ]);
+      ])
+    .then(() => {
+      return knex('post').insert([
+        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
+        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
+        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
+        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
+      ])
+    })
     });
 };
