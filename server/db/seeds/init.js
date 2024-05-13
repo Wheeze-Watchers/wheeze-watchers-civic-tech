815d5d2c4b5ed04a1cb9a24e57136cf1d51ea406
mayayr115 <mayayr115@gmail.com>
2024-05-13 12:56:40 -0400

    Accepted Current Change

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -7,22 +7,21 @@
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  return knex('user').del()
-    .then(() => {
-      // Inserts seed entries
-      return knex('user').insert([
-        {first_name: 'bob', last_name: 'dylan', email: 'bobdylan@mail.com', username: 'john_doe', password_hash: 'hashed_password_1', expert: true},
-        {first_name: 'bob', last_name: 'dylan', email: 'bobdylan2@mail.com', username: 'john_doe2', password_hash: 'hashed_password_1', expert: true},
-        {first_name: 'bob', last_name: 'dylan', email: 'bobdylan3@mail.com', username: 'john_doe3', password_hash: 'hashed_password_1', expert: true},
-        {username: 'jane_doe', password_hash: 'hashed_password_2'}
-      ])
-    .then(() => {
-      return knex('post').insert([
-      {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
-        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
-        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
-        {title: 'a title about something', body: 'body body body kajbfjasfgsfbal'},
-      ])
-    })
-    });
+  await knex('user').del();
+  await knex('post').del();
+  
+  // Inserts seed entries
+  await knex('user').insert([
+    { first_name: 'bob', last_name: 'dylan', email: 'bobdylan@mail.com', username: 'john_doe', password_hash: 'hashed_password_1', expert: true },
+    { first_name: 'bob', last_name: 'dylan', email: 'bobdylan2@mail.com', username: 'john_doe2', password_hash: 'hashed_password_1', expert: true },
+    { first_name: 'bob', last_name: 'dylan', email: 'bobdylan3@mail.com', username: 'john_doe3', password_hash: 'hashed_password_1', expert: true },
+    { username: 'jane_doe', password_hash: 'hashed_password_2' },
+  ]);
+
+  await knex('post').insert([
+    { title: 'a title about something', body: 'body body body kajbfjasfgsfbal' },
+    { title: 'a title about something', body: 'body body body kajbfjasfgsfbal' },
+    { title: 'a title about something', body: 'body body body kajbfjasfgsfbal' },
+    { title: 'a title about something', body: 'body body body kajbfjasfgsfbal' },
+  ]);
 };
