0c3bf7633a3278b77e02d14f03dbdfaa2253c7c6
mayayr115 <mayayr115@gmail.com>
2024-05-14 15:58:33 -0400

    Added Resource Button

--- frontend/src/pages/MemberResources.jsx
+++ frontend/src/pages/MemberResources.jsx
@@ -1,33 +1,45 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
 import CurrentUserContext from "../contexts/current-user-context";
 import Microlink from '@microlink/react'
 
 export default function () {
     const { currentUser } = useContext(CurrentUserContext)
-    console.log(currentUser)
-    // if (currentUser.expert === true) {
-    //     console.log(currentUser)
-    //    return <Button>Add</Button>
-    // }
+    const currentUserDummy = {
+      first_name: "bob",
+      last_name: "dylan",
+      email: "bobdylan@mail.com",
+      username: "john_doe",
+      expert: true,
+    }
+    const onClick = () => {
+        // code for pop up
+        console.log('function works')
+        
+    }
+    // console.log(currentUser)
     return (
         <>
         <div className="topic-container">
             <h1>Member Resources</h1>
         </div>
 
+        {currentUserDummy.expert &&<div id='resource'>
+            <button onClick={onClick}>Add Resource</button>
+         </div>
+         }
 
         <div className="info-box-container">
             <div className="info-box">
-                <Microlink url="https://www.uchicagomedicine.org/forefront/pediatrics-articles/2024/january/improving-asthma-outcomes-and-reducing-health-disparities"/>
+                <Microlink id="article-header" url="https://www.uchicagomedicine.org/forefront/pediatrics-articles/2024/january/improving-asthma-outcomes-and-reducing-health-disparities"/>
             </div>
 
             <div className="info-box">
-                <Microlink url="https://www.medicalnewstoday.com/articles/running-with-asthma"/>
+                <Microlink id="article-header" url="https://www.medicalnewstoday.com/articles/running-with-asthma"/>
             </div>
 
             <div className="info-box">
-                <Microlink url="https://www.who.int/news-room/fact-sheets/detail/asthma"/>
+                <Microlink id="article-header" url="https://www.who.int/news-room/fact-sheets/detail/asthma"/>
             </div>
         </div>
         <footer>
