d39be76e0ed356bb6df8afe814d9dacdbb8b5743
Aaron C <aaronc@MacBook-Pro-9.local>
2024-08-27 16:17:20 -0400

    resources bug fix

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -1,131 +1,149 @@
-import { useContext, useState, useEffect } from 'react';
-import { Form, useNavigate } from 'react-router-dom';
-import CurrentUserContext from '../contexts/current-user-context';
-import Microlink from '@microlink/react';
-import { fetchHandler, getPostOptions } from '../utils';
+import { useContext, useState, useEffect } from "react";
+import { Form, useNavigate } from "react-router-dom";
+import CurrentUserContext from "../contexts/current-user-context";
+import Microlink from "@microlink/react";
+import { fetchHandler, getPostOptions } from "../utils";
 
 export default function () {
-	const { currentUser } = useContext(CurrentUserContext);
-	const [newUrl, setNewUrl] = useState('')
-	const [resource, setResource] = useState([])
-	const [toggle, setToggle] = useState(true);
-	const [renderToggle, setRenderToggle] = useState(0);
-	const [usedUrl, setUsedUrl] = useState(new Set());
-	useEffect(() => {
-		const fetchResources = async () => {
-			const response = await fetchHandler('/api/resources/');
-			const validResources = response.filter(resource => resource !== null && resource !== undefined);
-			console.log(validResources[0])
-			const urls = validResources[0].map(resource => resource.url);
-			const uniqueUrls = new Set(urls);
-			setUsedUrl(uniqueUrls);
-			setResource(validResources[0]);
-		};
-		fetchResources()
-	}, [toggle, renderToggle])
+  const { currentUser } = useContext(CurrentUserContext);
+  const [newUrl, setNewUrl] = useState("");
+  const [resource, setResource] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [renderToggle, setRenderToggle] = useState(0);
+  const [usedUrl, setUsedUrl] = useState(new Set());
+  useEffect(() => {
+    const fetchResources = async () => {
+      const response = await fetchHandler("/api/resources/");
+      const validResources = response.filter(
+        (resource) => resource !== null && resource !== undefined
+      );
+      console.log(validResources[0]);
+      const urls = validResources[0].map((resource) => resource.url);
+      const uniqueUrls = new Set(urls);
+      setUsedUrl(uniqueUrls);
+      setResource(validResources[0]);
+    };
+    fetchResources();
+  }, [toggle, renderToggle]);
 
-	
-	const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-	const toggleModal = () => {
-		setIsActive(!isActive);
-	};
+  const toggleModal = () => {
+    setIsActive(!isActive);
+  };
 
-	const handleSubmit = async (e) => {
-	    e.preventDefault();
-		// console.log(usedUrl)
-		// console.log(usedUrl.has(newUrl))
-		toggleModal()
-	    if (currentUser && !usedUrl.has(newUrl)) {
-			
-			const response = await fetchHandler('/api/resources/', getPostOptions({ user_id: currentUser.id, url: newUrl }));
-			if (response[0] === null) {
-                throw new Error("Network response was not ok");
-            }
-			setRenderToggle(renderToggle + 1)
-			
-			newUrl=useState('')
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(usedUrl)
+    // console.log(usedUrl.has(newUrl))
+    toggleModal();
+    if (currentUser && !usedUrl.has(newUrl)) {
+      const response = await fetchHandler(
+        "/api/resources/",
+        getPostOptions({ user_id: currentUser.id, url: newUrl })
+      );
+      if (response[0] === null) {
+        throw new Error("Network response was not ok");
+      }
+      setRenderToggle(renderToggle + 1);
 
-	const handleDelete = async (id) => {
-		try {
-		  const response = await fetch(`/api/resources/${id}`, {
-			method: "DELETE",
-		  });
-		  if (!response.ok) {
-			throw new Error("Network response was not ok");
-		  }
-		  // ???
-		  setResource(resource.filter((resource) => resource.id !== id));
-		} catch (error) {
-		  console.error(
-			"There has been a problem with your fetch operation:",
-			error
-		  );
-		}
-		setToggle(!toggle)
-	  };
+      setNewUrl("");
+    }
+  };
 
-	return (
-		<>
-			<h1 className="title is-1 has-text-weight-bold has-text-centered my-6">
-				Resources
-			</h1>
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`/api/resources/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      // ???
+      setResource(resource.filter((resource) => resource.id !== id));
+    } catch (error) {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    }
+    setToggle(!toggle);
+  };
 
-			{currentUser && currentUser.expert && (
-				<>
-					<div className="buttons is-centered mb-6">
-						<button
-							className="button medium-blue-slate has-text-white has-text-weight-semibold"
-							onClick={toggleModal}
-						>
-							Add Link
-						</button>
-					</div>
-					
-				</>
-			)}
+  return (
+    <>
+      <h1 className="title is-1 has-text-weight-bold has-text-centered my-6">
+        Resources
+      </h1>
 
-			<div className="columns is-multiline is-half is-justify-content-center">
-			{resource && resource.map((val) => (
-				<div className="p-5 m-4" key={val.id}>
-					<div className="box is-justify-content-center">
-						<div className="content has-text-centered">
-							{val.url && typeof val.url === 'string' && (
-							<Microlink id={val.id} url={val.url} />
-							)}
-						</div>
-						{(currentUser && currentUser.expert && currentUser.id === val.user_id) && 
-						<button
-							type="button"
-							className="button is-danger has-text-weight-semibold"
-							onClick={() => handleDelete(val.id)}
-						>
-							Delete
-						</button>}
-					</div>
-				</div>
-			))}
-			</div>
+      {currentUser && currentUser.expert && (
+        <>
+          <div className="buttons is-centered mb-6">
+            <button
+              className="button medium-blue-slate has-text-white has-text-weight-semibold"
+              onClick={toggleModal}
+            >
+              Add Link
+            </button>
+          </div>
+        </>
+      )}
 
-			<div className={`modal ${isActive ? 'is-active' : ''}`}>
-				<div className="modal-background"></div>
-				<div className="modal-content">
-					<div className="box">
-						<form aria-labelledby="resource-form">
-							<label htmlFor="resource">Add Link Below:</label>
-							<input className="input" value={newUrl} type="text" id="resource-link" name="resource-link" onChange={(e) => setNewUrl(e.target.value)} />
-							<button className="button medium-blue-slate has-text-weight-semibold has-text-white" onClick={handleSubmit}>Add</button>
-						</form>
-					</div>
-				</div>
-				<button
-					className="modal-close is-large"
-					aria-label="close"
-					onClick={toggleModal}
-				></button>
-			</div>
-		</>
-	);
+      <div className="columns is-multiline is-half is-justify-content-center">
+        {resource &&
+          resource.map((val) => (
+            <div className="p-5 m-4" key={val.id}>
+              <div className="box is-justify-content-center">
+                <div className="content has-text-centered">
+                  {val.url && typeof val.url === "string" && (
+                    <Microlink id={val.id} url={val.url} />
+                  )}
+                </div>
+                {currentUser &&
+                  currentUser.expert &&
+                  currentUser.id === val.user_id && (
+                    <button
+                      type="button"
+                      className="button is-danger has-text-weight-semibold"
+                      onClick={() => handleDelete(val.id)}
+                    >
+                      Delete
+                    </button>
+                  )}
+              </div>
+            </div>
+          ))}
+      </div>
+
+      <div className={`modal ${isActive ? "is-active" : ""}`}>
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          <div className="box">
+            <form aria-labelledby="resource-form">
+              <label htmlFor="resource">Add Link Below:</label>
+              <input
+                className="input"
+                value={newUrl}
+                type="text"
+                id="resource-link"
+                name="resource-link"
+                onChange={(e) => setNewUrl(e.target.value)}
+              />
+              <button
+                className="button medium-blue-slate has-text-weight-semibold has-text-white"
+                onClick={handleSubmit}
+              >
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+        <button
+          className="modal-close is-large"
+          aria-label="close"
+          onClick={toggleModal}
+        ></button>
+      </div>
+    </>
+  );
 }
